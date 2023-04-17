5b500faa4047d88272c983efecb32e8b2779382d
Mzala123 <justicemwanzamj@gmail.com>
2023-04-17 11:02:07 +0200

    login

--- src/views/auth/login.vue
+++ src/views/auth/login.vue
@@ -1,7 +1,35 @@
 <template>
-  <div class="container mx-auto">
-    <p>Trial by fire</p>
-  </div>
+  
+    <div class="h-screen bg-gray_darkish  relative flex flex-col space-y-10 justify-center items-center">
+       <div class="bg-white md:shadow-md sm:shadow-none rounded p-6 w-96">
+            <h1 class="text-2xl font-bold leading-normal ">Welcome to</h1>
+            <div class="text-sm">Clientelle feedback app</div> 
+            <form class="space-y-5 mt-5">
+               <div class="mb-4 relative">
+                <input id="email" v-model="useremail" class="w-full rounded px-3 border border-gray-500 pt-5 pb-2 focus:outline-none input active:outline-none" type="text" autofocus>
+                <label for="email" class="label absolute mb-0 -mt-3.5 pt-4 pl-3 leading-tighter text-gray-500 text-base mt-2 cursor-text">Enter username</label>
+               </div>
+
+               <div class="relative flex items-center border border-gray-500 focus:ring focus:border-blue-500 rounded">
+                <input id="password" class="w-full rounded px-3 pt-5 outline-none pb-2 focus:outline-none active:outline-none input active:border-blue-500" type="password"/>
+                <label for="password" class="label absolute mb-0 -mt-3.5 pt-4 pl-3 leading-tighter text-gray-500 text-base mt-2 cursor-text">Password</label>
+                <a class="text-sm font-bold text-blue-700 hover:bg-blue-100 rounded-full px-2 py-1 mr-1 leading-normal cursor-pointer">show</a>
+               </div>
+
+               <div class="-m-2">
+                <a class="font-bold text-medium_light_blue hover:text-blue hover:underline p-2 rounded-full" href="#">Forgot password?</a>
+               </div>
+
+              <button @click="login" class="w-full text-center bg-medium_light_blue hover:bg-blue rounded-full text-white py-3 font-medium">Sign in
+                 
+              </button>
+
+              <p>Not a Customer?<a class="text-medium_light_blue font-semibold hover:text-blue hover:underline p-2 rounded-full" href="#">Register now</a></p>
+
+            </form>
+       </div>
+    </div>
+ 
 </template>
 
 <script>
@@ -9,8 +37,18 @@ import { ref } from "vue";
 
 export default {
   setup() {
+    const useremail = ref(null)
+    const password  = ref(null)
+
+    const login = ()=>{
+          
+    }
+
     return {
-      
+         useremail,
+         password,
+         login
+
     };
   },
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
      medium_light_blue:'#233868',
      sky:'#0ea5e9',
      navy_blue: '#0077a7',
+     gray_light: "#e5e7eb",
+     gray_darkish: '#e2e8f0',
      white: '#ffffff',
      white_slate:'#cbd5e1',
      light_orange:'#facc15',
