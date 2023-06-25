d28064be3225415cc2eb3f6744d51905d5cd4133
Mzala123 <justicemwanzamj@gmail.com>
2023-06-25 14:56:01 +0200

    eee

--- src/views/OurStaff.vue
+++ src/views/OurStaff.vue
@@ -12,9 +12,10 @@
             <div class="flex flex-col mt-24 md:flex-row md:space-x-6">
                 <!--Testimonial 1-->
                 <div class="flex flex-col items-center p-6 space-y-6 rounded-lg
-                bg-lighest md:w-1/3">
-                    <img src="../assets/img/jcole.jpg" class="w-48 -mt-14 rounded-full h-48 object-cover" alt="">
-                    <h5 class="text-lg font-bold">Anisha Li</h5>
+                bg-lighest md:w-1/3 mb-12">
+                    <img src="../assets/img/shuppie.jpeg" class="w-48 -mt-14 rounded-full h-48 object-cover" alt="">
+                    <h5 class="text-lg font-bold">Shuppie Kaoza</h5>
+                    <p class="text-sm font-bold"> Systems adminstrator</p>
                     <p class="text-sm text-darkGrayishBlue">
                         "Manage has superchanged our team's workflow. The ability to
                         maintain visibility on larger milestones at all times keeps
@@ -23,10 +24,11 @@
                 </div>
 
                 <!--Testimonial 2-->
-                <div class="hidden flex-col items-center p-6 space-y-6 rounded-lg
-              bg-lighest md:flex md:w-1/3">
-                    <img src="../assets/img/kendal.jpg" class="w-48 -mt-14 rounded-full h-48 object-cover" alt="">
-                    <h5 class="text-lg font-bold">Ali Bravo</h5>
+                <div class="flex flex-col items-center p-6 space-y-6 rounded-lg
+                bg-lighest md:w-1/3 mb-12">
+                    <img src="../assets/img/hyston.jpeg" class="w-48 -mt-14 rounded-full h-48 object-cover" alt="">
+                    <h5 class="text-lg font-bold">Hyston Kayange</h5>
+                    <p class="text-sm font-bold">Enquiry Personnel</p>
                     <p class="text-sm text-darkGrayishBlue">
                         "Manage has superchanged our team's workflow. The ability to
                         maintain visibility on larger milestones at all times keeps
@@ -35,10 +37,11 @@
                 </div>
 
                 <!--Testimonial 3-->
-                <div class="hidden flex-col items-center p-6 space-y-6 rounded-lg
-            bg-lighest md:flex md:w-1/3">
-                    <img src="../assets/img/profile.png" class="w-48 -mt-14 rounded-full h-48 object-cover" alt="">
-                    <h5 class="text-lg font-bold">Richard Watts</h5>
+                <div class="flex flex-col items-center p-6 space-y-6 rounded-lg
+                bg-lighest md:w-1/3  mb-12 ">
+                    <img src="../assets/img/lolly.jpeg" class="w-48 -mt-14 rounded-full h-48 object-cover" alt="">
+                    <h5 class="text-lg font-bold"> Lolly Mkandawire</h5>
+                    <p class="text-sm font-bold">Enquiry Personnel</p>
                     <p class="text-sm text-darkGrayishBlue">
                         "Manage has superchanged our team's workflow. The ability to
                         maintain visibility on larger milestones at all times keeps
